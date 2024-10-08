de34de97a09910541712f7136ab513244f420859
vvv-e <vvedensky@subi.su>
2024-10-08 13:20:01 +0500

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -15,10 +15,10 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
     if not is_adr_normal(recipient) or not is_adr_normal(sender):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
         return
-    elif recipient.upper() == sender.upper():
+    elif recipient.lower() == sender.lower():
         print("Нельзя отправить письмо самому себе!")
         return
-    elif sender == "university.help@gmail.com":
+    elif sender.lower() == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
         return
     else:
