f6e87a493f78c92aa8d082a6731292a93cbae0a7
vvv-e <vvedensky@subi.su>
2024-10-09 12:21:55 +0500

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -7,10 +7,10 @@ print_params()
 print_params(b=25)
 print_params(c=[1, 2, 3])
 
-values_list = [1, 'two', True]
+values_list = [2, 'two', False]
 print_params(*values_list)
 
-values_dict = {'a': 1, 'b': 'two', 'c': True}
+values_dict = {'a': 2, 'b': 'two', 'c': False}
 print_params(**values_dict)
 
 values_list_2 = [54.32, 'Строка']
