2cc7f322924c23432a3809b40240438f6c57abed
vvv-e <vvedensky@subi.su>
2024-10-11 12:43:08 +0500

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -5,7 +5,7 @@ def get_multiplied_digits(number):
     if len(str_number) > 1:
         first = int(str_number[0:1])
         return first * get_multiplied_digits(int(str_number[1:]))
-    elif len(str_number) <= 1:
+    else:
         return int(str_number[0:1])
 
 result = get_multiplied_digits(40203)
