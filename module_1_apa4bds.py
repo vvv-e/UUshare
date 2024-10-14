09cbf73876018873fa76b4dcf416b3a2597e3967
vvv-e <vvedensky@subi.su>
2024-10-15 01:09:54 +0500

    Update module_1_apa4bds.py

--- module_1_apa4bds.py
+++ module_1_apa4bds.py
@@ -6,17 +6,13 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
 stu_avg = {}               # создать пустой соварь
 students = sorted( list( students ) ) # преобразовать в лист и сортировать
-# print( students )                   # "отладочная печать" отсортированных студентов
 i = 0                                 # индекс студента
 for item_s in students:
-    # print(item_s, end=" ")          # "отладочная печать" имени студента
     average = 0.                      # средняя оценка
     for items_d in grades[i]:
-        # print(items_d, end=" ")     # "отладочная печать" оценок студента
         average += float(items_d)
     average /= float(len(grades[i]))
     i += 1
-    # print( average )                # "отладочная печать" средней оценки студента и возврат каретки
     stu_avg[item_s] = average         # добавить в словарь студента и среднюю оценку
 print( stu_avg )           # вывод результата в консоль
 
