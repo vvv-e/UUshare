e7cae0abad6a19ffcc93e43c3a63dc82c17317b1
vvv-e <vvedensky@subi.su>
2024-10-17 11:56:56 +0500

    Create module_2_3bis.py

---
+++ module_2_3bis.py
@@ -0,0 +1,8 @@
+# Стиль кода часть II. Цикл While.
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i < len(my_list) and my_list[i] >= 0:
+    if my_list[i] > 0:
+        print(my_list[i])
+    i += 1
