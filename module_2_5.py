48ef933b8e61e2e283ac44a5978233b16a9fb96f
vvv-e <vvedensky@subi.su>
2024-10-17 12:20:14 +0500

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,14 +1,15 @@
 # Функции в Python.Функция с параметром.
 
-def get_matrix( n = 0, m = 0, value = 0 ):
+def get_matrix(n=0, m=0, value=0):
     matrix = []
     if n < 0 or m < 0:
         return matrix
-    for i in range( 0, n ):
-        matrix.append( [] )
-        for j in range( 0, m ):
-            matrix[i].append( value )
+    for i in range(0, n):
+        matrix.append([])
+        for j in range(0, m):
+            matrix[i].append(value)
     return matrix
+
 
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
