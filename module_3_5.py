a952e3a509836208ac989bfe1c30cbf57088ba16
vvv-e <vvedensky@subi.su>
2024-10-19 14:38:46 +0500

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,4 +1,5 @@
 # Самостоятельная работа по уроку "Рекурсия".
+# Напиши функцию get_multiplied_digits, которая принимает аргумент целое число number и подсчитывает произведение цифр этого числа.
 
 def get_multiplied_digits(number):
     str_number = str(number)
@@ -6,7 +7,10 @@ def get_multiplied_digits(number):
         first = int(str_number[0:1])
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        return int(str_number[0:1])
+        return int(str_number[0:1]) if int(str_number[0:1]) > 1 else 1
 
-result = get_multiplied_digits(40203)
-print(result)
+if __name__ == '__main__':
+    result = get_multiplied_digits(40203)
+    print(result)
+    result = get_multiplied_digits(420)
+    print(result)
