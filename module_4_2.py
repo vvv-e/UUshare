a9178b9d94d24c0fa94e733ea0d657a9f0799af0
vvv-e <vvedensky@subi.su>
2024-10-24 09:55:49 +0500

    Create module_4_2.py

---
+++ module_4_2.py
@@ -0,0 +1,12 @@
+# Домашняя работа по уроку "Пространство имен."
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+
+if __name__ == '__main__':
+    test_function()
+    # inner_function() - NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
