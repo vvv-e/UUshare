5f57257e923e891ed85bbf48fc5a03d06c8b3fea
vvv-e <vvedensky@subi.su>
2024-11-05 10:46:19 +0500

    refactor

--- modul4/module_4_1.py
+++ modul4/module_4_1.py
@@ -1,7 +1,7 @@
 # Домашняя работа по уроку "Модули и пакеты"
 
-from fake_math import divide as fake_divide
-from true_math import divide as true_divide
+from modul1.fake_math import divide as fake_divide
+from modul1.true_math import divide as true_divide
 
 if __name__ == '__main__':
     result1 = fake_divide(69, 3)
