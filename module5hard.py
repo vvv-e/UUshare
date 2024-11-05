48cb6771fabc90f8828242e1394e47ab0acdc61c
vvv-e <vvedensky@subi.su>
2024-11-05 16:56:10 +0500

    Update module5hard.py

---
+++ module5hard.py
@@ -0,0 +1,106 @@
+# Дополнительное практическое задание по модулю: "Классы и объекты."
+
+class UrTube:
+    """
+    Уртуб.
+    Атрибуты: users(список объектов User), videos(список объектов Video), current_user(текущий пользователь, User)
+    """
+
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None
+
+    def log_in(self, nickname, password):
+        evrika = False
+        hsh = 0
+        cu = None
+        for usr in self.users:
+            if nickname == usr.nickname:
+                cu = usr
+                hsh = usr.password
+                evrika = True
+                break
+        if evrika:
+            if hash(password) == hsh:
+                self.current_user = None
+
+
+        else:
+            print(f"Пользователь с именем {nickname} не существует.")
+
+    def register(self, nickname, password, age):
+        evrika = False
+        for usr in self.users:
+            if nickname == usr.nickname:
+                evrika = True
+                break
+        if evrika:
+            print(f"Пользователь {nickname} уже существует.")
+        else:
+            usr = User(nickname, password, age)
+            if not usr is None:
+                self.users.append(usr)
+                self.current_user = usr
+
+    def log_out(self):
+        self.current_user = None
+
+    def add(self, video):
+        pass
+
+    def get_videos(self, video):
+        pass
+
+    def watch_video(self, title):
+        pass
+
+
+class User:
+    """
+    Пользователь.
+    Атрибуты: nickname(имя пользователя, строка), password(хэш-код, число), age(возраст, число)
+    """
+
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(args[0], str):
+            print("Имя пользователя должно быть строковым. Объект Пользователь не создан.")
+        elif not isinstance(args[1], str):
+            print("Пароль должен быть строковым. Объект Пользователь не создан.")
+        elif not isinstance(args[1], int):
+            print("Возраст должен быть целым числом. Объект Пользователь не создан.")
+        else:
+            return super().__new__(cls)
+
+    def __init__(self, nickname, password, age):
+        self.nickname = nickname
+        self.password = hash(password)
+        self.age = age
+
+
+class Video:
+    """
+    Видео.
+    Атрибуты:title(заголовок, строка), duration(продолжительность, секунды), time_now(секунда остановки (изначально 0)),
+             adult_mode(ограничение по возрасту, bool (False по умолчанию))
+    """
+
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(args[0], str):
+            print("Название фильма должно быть строковым. Объект Видео не создан.")
+        elif not isinstance(args[1], int):
+            print("Продолжительность фильма должна быть целым числом. Объект Видео не создан.")
+        elif kwargs.get('adult_mode') != None and not isinstance(kwargs['adult_mode'], bool):
+            print("Ограничение по возрасту должно быть логическим значением. Объект Видео не создан.")
+        else:
+            return super().__new__(cls)
+
+    def __init__(self, title, duration, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.adult_mode = adult_mode
+
+
+if __name__ == '__main__':
+    v1 = Video('Лучший язык программирования 2024 года', 200)
+    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
