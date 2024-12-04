892cc289b8b99af8534d3eecd4ae14fe01b2f15c
vvv-e <vvedensky@subi.su>
2024-12-04 10:33:32 +0500

    Create module_9_6.py

---
+++ module09/module_9_6.py
@@ -0,0 +1,12 @@
+# Домашнее задание по теме "Генераторы"
+
+def all_variants(text):
+    for i in range(len(text)):
+        for j in range(len(text) - i):
+            yield text[j:j + i + 1]
+
+
+if __name__ == "__main__":
+    a = all_variants("abc")
+    for i in a:
+        print("." + i)
