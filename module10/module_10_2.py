7e0a6c4aac91b93ba5ae982333df4fc3d78728c8
vvv-e <vvedensky@subi.su>
2024-12-10 11:56:54 +0500

    Create module_10_2.py

---
+++ module10/module_10_2.py
@@ -0,0 +1,30 @@
+# Домашнее задание по теме "Потоки на классах"
+import threading
+from time import sleep
+
+class Knight(threading.Thread):
+    def __init__(self, name, power):
+        super().__init__()
+        self.name = name
+        self.power = power
+
+    def run(self):
+        print(f"{self.name}, на нас напали!\n",end="")
+        enemy = 100 # количество врагов
+        days = 0 # сражается дней
+        while enemy > 0:
+            days += 1
+            enemy -= self.power
+            print(f"{self.name} сражается {days} дней(дня)..., осталось {enemy} воинов.\n",end="")
+            sleep(1)
+        print(f"{self.name} одержал победу спустя {days} дней(дня)!\n",end="")
+
+if __name__ == "__main__":
+    first_knight = Knight('Sir Lancelot', 10)
+    second_knight = Knight("Sir Galahad", 20)
+    first_knight.start()
+    second_knight.start()
+    first_knight.join()
+    second_knight.join()
+    print("Все битвы закончились!")
+
