6bf6a702d053191094958d93bc4b4c2cb3caa657
vvv-e <vvedensky@subi.su>
2024-12-10 11:59:55 +0500

    Update module_10_2.py

--- module10/module_10_2.py
+++ module10/module_10_2.py
@@ -1,23 +1,25 @@
 # Домашнее задание по теме "Потоки на классах"
-import threading
+from threading import Thread
 from time import sleep
 
-class Knight(threading.Thread):
+
+class Knight(Thread):
     def __init__(self, name, power):
         super().__init__()
         self.name = name
         self.power = power
 
     def run(self):
-        print(f"{self.name}, на нас напали!\n",end="")
-        enemy = 100 # количество врагов
-        days = 0 # сражается дней
+        print(f"{self.name}, на нас напали!\n", end="")
+        enemy = 100  # количество врагов
+        days = 0  # сражается дней
         while enemy > 0:
             days += 1
             enemy -= self.power
-            print(f"{self.name} сражается {days} дней(дня)..., осталось {enemy} воинов.\n",end="")
+            print(f"{self.name} сражается {days} дней(дня)..., осталось {enemy} воинов.\n", end="")
             sleep(1)
-        print(f"{self.name} одержал победу спустя {days} дней(дня)!\n",end="")
+        print(f"{self.name} одержал победу спустя {days} дней(дня)!\n", end="")
+
 
 if __name__ == "__main__":
     first_knight = Knight('Sir Lancelot', 10)
@@ -27,4 +29,3 @@ if __name__ == "__main__":
     first_knight.join()
     second_knight.join()
     print("Все битвы закончились!")
-
