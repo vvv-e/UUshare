1ff872cdc6b7139d45266d03d8e1ba4e5e8c50f8
vvv-e <vvedensky@subi.su>
2024-12-23 14:06:43 +0500

    Update module_11_1.py

--- module 11/module_11_1.py
+++ module 11/module_11_1.py
@@ -1,4 +1,12 @@
 # Домашнее задание по теме "Обзор сторонних библиотек Python"
+import requests
+
 
 if __name__ == "__main__":
-    pass
+    ACCESS_TOKEN = 'CXyFeSBw2lAdG41xkuU3LS6a_nwyxwwCz2dCkUohw-rw0C49x2HqP__6_4is5RPx'
+    RANDOM_GENRE_API_URL = 'https://binaryjazz.us/wp-json/genrenator/v1/genre'
+    GENIUS_API_URL = 'https://api.genius.com/search'
+    GENIUS_URL = 'https://api.genius.com'
+    genre = requests.get(RANDOM_GENRE_API_URL).json()
+    print(genre)
+
