133b6fb5a2cc878fabcd443639fa49f248840d54
vvv-e <vvedensky@subi.su>
2025-01-09 02:15:16 +0500

    cr dz07.01

--- module12/tests_12_4.py
+++ module12/tests_12_4.py
@@ -34,3 +34,6 @@ class RunnerTest(ut.TestCase):
 
 logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_tests.log", encoding="UTF-8",
                     format="%(asctime)s; %(levelname)s; %(message)s.")
+
+if __name__ == "__main__":
+    ut.main()
